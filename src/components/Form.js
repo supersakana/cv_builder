ad58fbbd4f6db4ed46ace4548acde659fc4ecac6
supersakana <zacwilliamson@icloud.com>
2023-03-29 15:34:46 -0600

    installed tailwindcss and added basic styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,73 +1,73 @@
 const Form = () => {
   return (
     <form>
-      <h4>General</h4>
+      <h4 className="section-title">General</h4>
 
-      <div>
+      <div className="form-control">
         <label for='name'>Name</label>
         <input type='text' placeholder="John Doe" />
       </div>
 
-      <div>
+      <div className="form-control">
         <label for='email'>Email</label>
         <input type='email' placeholder='example@this.com' />
       </div>
 
-      <div>
+      <div className="form-control">
         <label for='phone'>Phone</label>
         <input type='phone' placeholder='+1 (123)-456-789' />
       </div>
 
-      <h4>Education</h4>
+      <h4 className="section-title">Education</h4>
 
-      <div>
+      <div className="form-control">
         <label for='university'>University</label>
         <input type='text' placeholder='University of Example' />
       </div>
 
-      <div>
+      <div className="form-control">
         <label for='degree'>Degree</label>
         <input type='text' placeholder='Degree Title' />
       </div>
 
-      <div>
+      <div className="form-control">
         <label for='from-uni'>From</label>
         <input type='date' />
       </div>
 
-      <div>
+      <div className="form-control">
         <label for='to-uni'>To</label>
         <input type='date' />
       </div>
 
-      <h4>Experience</h4>
+      <h4 className="section-title">Experience</h4>
 
-      <div>
+      <div className="form-control">
         <label for='company'>Company</label>
         <input type='text' placeholder='Company Name' />
       </div>
 
-       <div>
+       <div className="form-control">
         <label for='position'>Position</label>
         <input type='text' placeholder='Position Title' />
       </div>
 
-      <div>
+      <div className="form-control">
         <label for='company'>Description</label>
-        <textarea placeholder='List a description of your job here' />
+        <textarea placeholder='A description of your job here' />
       </div>
 
-      <div>
+      <div className="form-control">
         <label for='from-job-1'>From</label>
         <input type='date' />
       </div>
 
-      <div>
+      <div className="form-control">
         <label for='to-job-1'>To</label>
         <input type='date' />
       </div>
 
-      <input type='submit' value='Submit' />
+      <input type='submit' value='Submit' className="bg-green-500 text-white rounded-md py-1 px-2 hover:bg-green-400 duration-500" />
     </form>
   )
 }
