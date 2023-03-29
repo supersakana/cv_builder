a0ef4236dad21bbed99bc16042029bccb6c29827
supersakana <zacwilliamson@icloud.com>
2023-03-29 15:42:44 -0600

    prevents default action on submit, added title, and changed label's 'for' to 'htmlFor'

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,69 +1,75 @@
 const Form = () => {
+
+  const onSubmit = (e) =>{
+    e.preventDefault()
+    console.log(e)
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h4 className="section-title">General</h4>
 
       <div className="form-control">
-        <label for='name'>Name</label>
+        <label htmlFor='name'>Name</label>
         <input type='text' placeholder="John Doe" />
       </div>
 
       <div className="form-control">
-        <label for='email'>Email</label>
+        <label htmlFor='email'>Email</label>
         <input type='email' placeholder='example@this.com' />
       </div>
 
       <div className="form-control">
-        <label for='phone'>Phone</label>
+        <label htmlFor='phone'>Phone</label>
         <input type='phone' placeholder='+1 (123)-456-789' />
       </div>
 
       <h4 className="section-title">Education</h4>
 
       <div className="form-control">
-        <label for='university'>University</label>
+        <label htmlFor='university'>University</label>
         <input type='text' placeholder='University of Example' />
       </div>
 
       <div className="form-control">
-        <label for='degree'>Degree</label>
+        <label htmlFor='degree'>Degree</label>
         <input type='text' placeholder='Degree Title' />
       </div>
 
       <div className="form-control">
-        <label for='from-uni'>From</label>
+        <label htmlFor='from-uni'>From</label>
         <input type='date' />
       </div>
 
       <div className="form-control">
-        <label for='to-uni'>To</label>
+        <label htmlFor='to-uni'>To</label>
         <input type='date' />
       </div>
 
       <h4 className="section-title">Experience</h4>
 
       <div className="form-control">
-        <label for='company'>Company</label>
+        <label htmlFor='company'>Company</label>
         <input type='text' placeholder='Company Name' />
       </div>
 
        <div className="form-control">
-        <label for='position'>Position</label>
+        <label htmlFor='position'>Position</label>
         <input type='text' placeholder='Position Title' />
       </div>
 
       <div className="form-control">
-        <label for='company'>Description</label>
+        <label htmlFor='company'>Description</label>
         <textarea placeholder='A description of your job here' />
       </div>
 
       <div className="form-control">
-        <label for='from-job-1'>From</label>
+        <label htmlFor='from-job-1'>From</label>
         <input type='date' />
       </div>
 
       <div className="form-control">
-        <label for='to-job-1'>To</label>
+        <label htmlFor='to-job-1'>To</label>
         <input type='date' />
       </div>
 
