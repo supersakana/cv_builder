84829c245cb3b47cdbccbf950bc7b3850becbd2e
supersakana <zacwilliamson@icloud.com>
2023-03-29 15:09:56 -0600

    added education section (university, degree, start, end)

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,8 @@
 const Form = () => {
   return (
     <form>
+      <h4>General</h4>
+
       <div>
         <label for='name'>Name</label>
         <input type='text' placeholder="John Doe" />
@@ -14,6 +16,28 @@ const Form = () => {
       <div>
         <label for='phone'>Phone</label>
         <input type='phone' placeholder='+1 (123)-456-789' />
+      </div>
+
+      <h4>Education</h4>
+
+      <div>
+        <label for='university'>University</label>
+        <input type='text' placeholder='University of Example' />
+      </div>
+
+      <div>
+        <label for='degree'>Degree</label>
+        <input type='text' placeholder='Degree Title' />
+      </div>
+
+      <div>
+        <label for='from'>From</label>
+        <input type='date' />
+      </div>
+
+      <div>
+        <label for='to'>To</label>
+        <input type='date' />
       </div>
 
       <input type='submit' value='Submit' />
