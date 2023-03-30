79b03bf2e68081e93cc413709679d779259ff5ab
supersakana <zacwilliamson@icloud.com>
2023-03-30 17:17:22 -0600

    added updateDoc functionality to each form input with ids

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,6 @@ const Form = ({ updateDoc }) => {
 
   const onSubmit = (e) =>{
     e.preventDefault()
-    console.log(e)
   }
 
   return (
@@ -16,64 +15,64 @@ const Form = ({ updateDoc }) => {
 
       <div className="form-control">
         <label htmlFor='email'>Email</label>
-        <input type='email' placeholder='example@this.com' />
+        <input id='email' type='email' placeholder='example@this.com' onChange={updateDoc} />
       </div>
 
       <div className="form-control">
         <label htmlFor='phone'>Phone</label>
-        <input type='phone' placeholder='+1 (123)-456-789' />
+        <input id='phone' type='phone' placeholder='+1 (123)-456-789' onChange={updateDoc} />
       </div>
 
       <h4 className="section-title">Education</h4>
 
       <div className="form-control">
         <label htmlFor='university'>University</label>
-        <input type='text' placeholder='University of Example' />
+        <input id='university' type='text' placeholder='University of Example' onChange={updateDoc} />
       </div>
 
       <div className="form-control">
         <label htmlFor='degree'>Degree</label>
-        <input type='text' placeholder='Degree Title' />
+        <input id='degree' type='text' placeholder='Degree Title' onChange={updateDoc} />
       </div>
 
       <div className="form-control">
-        <label htmlFor='from-uni'>From</label>
-        <input type='date' />
+        <label htmlFor='from_uni'>From</label>
+        <input id='from_uni' type='date' onChange={updateDoc} />
       </div>
 
       <div className="form-control">
-        <label htmlFor='to-uni'>To</label>
-        <input type='date' />
+        <label htmlFor='to_uni'>To</label>
+        <input id='to_uni' type='date' onChange={updateDoc} />
       </div>
 
       <h4 className="section-title">Experience</h4>
 
       <div className="form-control">
         <label htmlFor='company'>Company</label>
-        <input type='text' placeholder='Company Name' />
+        <input id='company' type='text' placeholder='Company Name' onChange={updateDoc} />
       </div>
 
        <div className="form-control">
         <label htmlFor='position'>Position</label>
-        <input type='text' placeholder='Position Title' />
+        <input id='position' type='text' placeholder='Position Title' onChange={updateDoc} />
       </div>
 
       <div className="form-control">
         <label htmlFor='company'>Description</label>
-        <textarea placeholder='A description of your job here' />
+        <textarea id='description' placeholder='A description of your job here' onChange={updateDoc} />
       </div>
 
       <div className="form-control">
-        <label htmlFor='from-job-1'>From</label>
-        <input type='date' />
+        <label htmlFor='from_comp'>From</label>
+        <input id='from_comp' type='date' onChange={updateDoc} />
       </div>
 
       <div className="form-control">
-        <label htmlFor='to-job-1'>To</label>
-        <input type='date' />
+        <label htmlFor='to_comp'>To</label>
+        <input id='to_comp' type='date' onChange={updateDoc} />
       </div>
 
-      <input type='submit' value='Submit' className="bg-green-500 text-white rounded-md py-1 px-2 hover:bg-green-400 duration-500" />
+      <input type='submit' value='Submit' className="bg-blue-500 text-white rounded-md py-1 px-2 hover:bg-blue-400 duration-500" />
     </form>
   )
 }
