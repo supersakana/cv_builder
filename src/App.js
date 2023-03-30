1e687b957ccb8f8c28a92541b7a4f996f0f15551
supersakana <zacwilliamson@icloud.com>
2023-03-30 11:24:01 -0600

    added simple doc section where the form output will display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Form from "./components/Form";
+import Doc from "./components/Doc";
 
 function App() {
   return (
-    <div>
+    <div className="flex">
       <Form />
+      <Doc />
     </div>
   );
 }
---
+++ src/components/Doc.js
@@ -0,0 +1,26 @@
+const Doc = () => {
+  return (
+    <div>
+      <p className="font-bold">Name</p>
+      <p className="font-bold">Email</p>
+      <p className="font-bold">Phone</p>
+
+      <br></br>
+
+      <p className="font-bold">University</p>
+      <p className="font-bold">Degree</p>
+      <p className="font-bold">From</p>
+      <p className="font-bold">To</p>
+
+      <br></br>
+
+      <p className="font-bold">Company</p>
+      <p className="font-bold">Position</p>
+      <p className="font-bold">Description</p>
+      <p className="font-bold">From</p>
+      <p className="font-bold">To</p>
+    </div>
+  )
+}
+
+export default Doc
